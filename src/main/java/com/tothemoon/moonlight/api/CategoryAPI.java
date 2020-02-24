b06b5fa6cdc261525fe929b93479ed5af0737578
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-02-24 01:22:05 -0300

    adding h2, jpa, devtools, and lombok to project

--- src/main/java/com/tothemoon/moonlight/api/CategoryAPI.java
+++ src/main/java/com/tothemoon/moonlight/api/CategoryAPI.java
@@ -1,16 +1,33 @@
 package com.tothemoon.moonlight.api;
 
+import com.tothemoon.moonlight.core.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "categories")
 public class CategoryAPI {
 
   @GetMapping
-  public String toList() {
-    return "REST is working!";
+  public List<Category> toList() {
+
+    Category cat1 = Category.builder()
+        .name("baianinho")
+        .id(204)
+        .build();
+
+    Category cat2 = Category.builder()
+        .name("armando costa")
+        .id(456)
+        .build();
+
+    List<Category> list = Arrays.asList(cat1, cat2);
+
+    return list;
   }
 
 }
